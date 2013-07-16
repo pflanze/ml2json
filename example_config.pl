8e3d817996e159fe6df6a4544c9f4ed716b04158
Christian Jaeger <ch@christianjaeger.ch>
2013-07-16 17:14:14 +0100

    add --config option

---
+++ example_config.pl
@@ -0,0 +1,31 @@
++{
+  jsonfields_orig_headers=>
+  [
+   ["Return-Path"=> 1],
+   [Received=> 2],
+   [Date=> 1],
+   [From=> 1],
+   [To=> 1],
+   ["Message-ID"=> 1],
+   [Subject=> 1],
+   ["Mime-Version"=> 1],
+   ["Content-Type"=> 1],
+   ["Delivered-To"=> 1],
+   ["Received-SPF"=> 1],
+   ["Authentication-Results"=> 1],
+   ["User-Agent"=> 1],
+  ],
+  jsonfields_top=>
+  [
+   ["orig_headers"=> "json_orig_headers", 2],
+   ["message-id"=> "json_message_id",1],
+   [replies=> "json_replies", 2],
+   ["in-reply-to"=> "json_in_reply_to", 1],
+   [unixtime=> "json_unixtime", 1],
+   [ctime_UTC=> "json_ctime_UTC", 1],
+   [orig_plain=> "json_orig_plain", 2],
+   [orig_html=> "json_orig_html", 2],
+   [html=> "json_html", 2],
+   [attachments=> "json_attachments", 2],
+  ]
+ }
