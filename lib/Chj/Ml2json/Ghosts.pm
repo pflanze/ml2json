37c3f4df87127b33dc32963f0708a09a61258fad
Christian Jaeger <ch@christianjaeger.ch>
2014-06-14 20:42:12 +0100

    Maildir: add ghost_make_, ghost_make

--- lib/Chj/Ml2json/Ghosts.pm
+++ lib/Chj/Ml2json/Ghosts.pm
@@ -20,11 +20,13 @@ Ghosting intermediate classes (above Chj::Ghostable, below Mailcollections).
 package Chj::Ml2json::Ghosts;
 
 @ISA="Exporter"; require Exporter;
-@EXPORT=qw(ghost_path);
+@EXPORT=qw(ghost_path ghost_make_ ghost_make);
 @EXPORT_OK=qw();
 %EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
 
 use strict;
+
+use Chj::xperlfunc qw(xLmtime XLmtime);
 
 our $maybe_cache_dir;
 # either undef or path string to base directory
@@ -42,6 +44,36 @@ sub ghost_path {
     }
 }
 # tests, proof?
+
+# Like "make", expect outputfile, sourcefile, generation code (IO
+# function returning value, not ghost; is turned into a ghost before
+# being returned, though)
+
+sub ghost_make_ ($$$;$) {
+    my ($targetpath, $sourcepath_mtime_thunk, $generate, $maybe_ghostclass)=@_;
+    my $Do= sub {
+	&$generate ()->ghost($targetpath)
+    };
+    if (defined (my $targetmtime= XLmtime (ghost_path $targetpath))) {
+	my $sourcemtime= &$sourcepath_mtime_thunk ();
+	if ($targetmtime > $sourcemtime) {
+	    ($maybe_ghostclass || "Chj::Ml2json::Ghost")->new($targetpath);
+	} else {
+	    &$Do
+	}
+    } else {
+	&$Do
+    }
+}
+
+sub ghost_make ($$$;$) {
+    my ($targetpath, $sourcepath, $generate, $maybe_ghostclass)=@_;
+    ghost_make_
+      ($targetpath,
+       sub { xLmtime ($sourcepath) },
+       $generate,
+       $maybe_ghostclass);
+}
 
 
 {
